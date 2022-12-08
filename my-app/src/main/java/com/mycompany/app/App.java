512bce2f2db946d78e9d7aef05991ffa61f4fd23
Elhay Efrat <elhayefrat@gmail.com>
2022-12-08 07:53:10 +0200

    Update App.java
    Change Hello World mesg

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "So here is the change right? !" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
